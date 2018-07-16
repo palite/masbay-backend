7e83e719e7b7780dc1c5e61bca2c4ad73403e285
muhabibull <mhabibullah14@yahoo.com>
2018-07-16 11:39:01 +0700

    log database mlab

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise;
-clusterDB = process.env.DATABASE//.substring(process.env.DATABASE.indexOf('@'), process.env.DATABASE.indexOf('-'));
+clusterDB = process.env.DATABASE.substring(process.env.DATABASE.indexOf('@'), process.env.DATABASE.indexOf('.'));
 mongoose.connection
     .on('connected', () => {
         console.log(`Mongoose connection open on ${clusterDB}`);
