66171c0acef4791fc15e669d1b3b17faf0e0bcba
palite <kukuh.raharjo1923@gmail.com>
2018-07-24 18:36:23 +0700

    notif debug gara2 }}}}}}}}}}}}}}}}}}}}}}}}

--- controllers/notifController.js
+++ controllers/notifController.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const Transaksi = mongoose.model('Transaksi');
-
+const Transaksi1 = mongoose.model('Transaksi');
 const TopUp = mongoose.model('TopUp');
+const TopUp1 = mongoose.model('TopUp');
 var moment = require('moment');
 exports.notif = function  () {
     console.log('hoo');
@@ -18,6 +19,17 @@ exports.notif = function  () {
             var hourDiff = diff.asMinutes();
             if (hourDiff <=63 && hourDiff >= 58) {
                 console.log('wooooooooooy isi pulsamu');
+            } else if (data[i].status === 'Expired') {
+                Transaksi.findOneAndUpdate({price : data[i].price,status : "Expired"},{$set:{status:"Late"}},function (err,info)  {
+                    if (err) {
+                        console.log("update eror");
+                    } else {
+                        console.log("update berhasil");
+                    }
+                    console.log("hai");
+                });
+                // Transaksi.update({price: data[i].price},{$set:{status: "terlalu_lama"}});
+                    console.log('WOOY LEWAT');
             }
         }
     }) 
@@ -28,7 +40,18 @@ exports.notif = function  () {
             var end = moment(data[i].date,'YYYY-M-DD HH:mm:ss');
             var hourDiff = end.diff(now,"m");
             if (hourDiff <=63 && hourDiff >= 58) {
-                console.log('wooooooooooy isi pulsamu');
+                console.log('wooooooooooy isi saldomu');
+            } else if (data[i].status === 'Expired') {
+               TopUp.findOneAndUpdate({price : data[i].price,status: "Expired"},{$set:{status:"Late"}},function (err,info)  {
+                   if (err) {
+                       console.log("update eror");
+                   } else {
+                       console.log("update berhasil");
+                   }
+                   
+               });
+                // TopUp.update({price: data[i].price},{status: "terlalu_lama"});
+                console.log('WOOY saldo LEWAT');
             }
         }
     }) 
