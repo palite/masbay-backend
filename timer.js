b786c41fc40b43aa6426cb281b7e796648d493e3
muhabibull <mhabibullah14@yahoo.com>
2018-07-26 13:16:20 +0700

    scheduler + setinterval

--- timer.js
+++ timer.js
@@ -7,34 +7,43 @@ mongoose.Promise = global.Promise;
 clusterDB = process.env.DATABASE.substring(process.env.DATABASE.indexOf('@'), process.env.DATABASE.indexOf('?'));
 mongoose.connection
     .on('connected', () => {
-        console.log(`TIMER - Mongoose connection open on ${clusterDB}`);
+        console.log(`SCHEDULER - Mongoose connection open on ${clusterDB}`);
     })
     .on('error', (err) => {
-        console.log(`TIMER - Connection error: ${err.message}`);
+        console.log(`SCHEDULER - Connection error: ${err.message}`);
     });
 
 require('./models/index');
 
-//var notif_controller = require('./controllers/notifController');
-//notif_controller.notif();
-
 var transaksi_controller = require('./controllers/transaksiController');
 var topup_controller = require('./controllers/topUpController');
+var notif_controller = require('./controllers/notifController');
+var crawl_controller = require('./controllers/crawlController');
 
 transaksi_controller.updateStatusTransaksi();
 topup_controller.updateStatusTopUp();
-setInterval(transaksi_controller.updateStatusTransaksi, 30000); //req setiap x/1000 detik
-setInterval(topup_controller.updateStatusTopUp, 30000); //req setiap x/1000 detik
+notif_controller.notif();
+setInterval(transaksi_controller.updateStatusTransaksi, 10000); //req setiap x/1000 detik
+setInterval(topup_controller.updateStatusTopUp, 10000); //req setiap x/1000 detik
+setInterval(notif_controller.notif, 300000); //req setiap x/1000 detik
 
 
-var crawl_controller = require('./controllers/crawlController');
-crawl_controller.crawler(cekmutasi => {
-    crawl_controller.transaksi(cekmutasi, sudah => {
-        crawl_controller.topup(cekmutasi, sudah2 => {
-            //if ((sudah) && (sudah2)) {
-                console.log('sudah selesai crawl');
-                //process.exit();
-            //}
+
+function crawl() {
+    crawl_controller.crawler(cekmutasi => {
+        crawl_controller.transaksi(cekmutasi, sudah => {
+            crawl_controller.topup(cekmutasi, sudah2 => {
+                
+            });
         });
-    });
-})
+    })
+}
+
+function stop() {
+    date = new Date();
+    console.log(date);
+    process.exit();
+}
+
+setInterval(crawl, 180000); //3 menit
+setInterval(stop, 590000); //9 menit 50 detik
