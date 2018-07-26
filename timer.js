f15da4876a6fb6c2abbf4d8320d9b2120986e370
muhabibull <mhabibullah14@yahoo.com>
2018-07-26 10:21:03 +0700

    timer update status

--- timer.js
+++ timer.js
@@ -18,14 +18,23 @@ require('./models/index');
 //var notif_controller = require('./controllers/notifController');
 //notif_controller.notif();
 
+var transaksi_controller = require('../controllers/transaksiController');
+var topup_controller = require('../controllers/topUpController');
+
+transaksi_controller.updateStatusTransaksi();
+topup_controller.updateStatusTopUp();
+setInterval(transaksi_controller.updateStatusTransaksi, 30000); //req setiap x/1000 detik
+setInterval(topup_controller.updateStatusTopUp, 30000); //req setiap x/1000 detik
+
+
 var crawl_controller = require('./controllers/crawlController');
 crawl_controller.crawler(cekmutasi => {
     crawl_controller.transaksi(cekmutasi, sudah => {
         crawl_controller.topup(cekmutasi, sudah2 => {
-            if ((sudah) && (sudah2)) {
+            //if ((sudah) && (sudah2)) {
                 console.log('sudah selesai crawl');
-                process.exit();
-            }
+                //process.exit();
+            //}
         });
     });
 })
