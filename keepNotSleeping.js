457422a68dc9df60ff45b9eb098f35bbc4c1ae12
muhabibull <mhabibullah14@yahoo.com>
2018-07-26 16:33:40 +0700

    edit url kembali

--- keepNotSleeping.js
+++ keepNotSleeping.js
@@ -2,12 +2,12 @@ function keepNotSleeping() {
     var request = require("request");
     var options = {
         method: 'GET',
-        url: process.env.URLKEEPNOTSLEEP,
+        url: 'https://intense-inlet-67504.herokuapp.com/faq',
         headers: 
         { 'Cache-Control': 'no-cache' }
     };
     request(options, function (error, response, body) {
-        //console.log(body);
+        console.log(body);
     })
 }
 function stop() {
