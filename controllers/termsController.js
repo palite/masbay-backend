9831231ed0fce027fcfd4ee0bcf6e7174036335b
muhabibull <mhabibullah14@yahoo.com>
2018-08-10 15:44:07 +0700

    terms urutin

--- controllers/termsController.js
+++ controllers/termsController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Terms = mongoose.model('Terms');
 exports.term = function (callback) {
-    Terms.find().sort({Alfabet: 1,Nomor:1})
+    Terms.find().sort({alfabet:1, nomor:1})
     .then((listTerm) => {
        // listTerm.find().sort({Nomor: 1})
         //.then((list2Term)) => {
